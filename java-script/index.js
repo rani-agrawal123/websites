f15170c9f019c026570ea3a4b326d3d3acd3439d
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-08-20 20:21:35 +0530

    add new

---
+++ java-script/index.js
@@ -0,0 +1,7 @@
+alert("hay")
+console.log("console is running....")
+console.log("console is  also running....")
+
+var a = prompt("enter your no.")
+console.log(a)
+console.log("your number is" +a)
