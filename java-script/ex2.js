bd285d834a13bd300a977b6074a87a4141b038cf
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-08-30 12:06:40 +0530

    add

---
+++ java-script/ex2.js
@@ -0,0 +1,49 @@
+// this is ex-10 and tutorial 64
+// adjective:
+// crazy ,amazing, fire
+
+// shope name:
+// engine, foods, garments
+
+// another word:
+// Bros, limited, hub
+
+let rand = Math.random()
+let first, second, third;
+// 0, 0.33, 0.66, 1
+
+// lets genret the first word
+if(rand<0.33){
+    first = "crazy"
+}
+else if(rand<0.66 && rand>=0.33){
+    first = "amazing"
+}
+else{
+    first = "fire"
+}
+
+// lets generate the second word
+rand = Math.random()
+if(rand<0.33){
+    second = "bros"
+}
+else if(rand<0.66 && rand>=0.33){
+    second = "limited"
+}
+else{
+    second = "hub"
+}
+// lets generate the other word
+rand = Math.random()
+if(rand<0.66){
+    third = "engine"
+}
+else if(rand<0.66 && rand>=1){
+    third = "foods"
+}
+else{
+    third = "garments"
+}
+
+console.log(`${first} ${second} ${third}`);
---
+++ java-script/string.js
@@ -0,0 +1,21 @@
+console.log("this is string tutorial");
+let a = "harry";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[5]);
+console.log(a.length);
+
+let _name = "harry";
+let friend = "rohan";
+console.log("His name is " + _name +" and his friend is " + friend);
+console.log(`His name is ${_name} and his friend is ${friend}`);
+console.log(friend.toUpperCase());
+
+let b = "shivam";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1,4));
+console.log(b.replace("sh","77"));
+console.log(b.concat(a," arav"));
